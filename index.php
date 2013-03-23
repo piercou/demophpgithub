1c06dbe6f40017fcf76375d37a665724cdb0ae88
piercou <pierre@couzy.com>
2013-03-23 15:57:58 +0100

    Update index.php
    modif joomla day

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <body>
         <form id="form1" >
         <div>
-            Un site plein de nouveaux trucs..<br/>
+            Un site plein de nouveaux nouveaux trucs..<br/>
             <?echo phpinfo();?>
         </div>
         </form>
